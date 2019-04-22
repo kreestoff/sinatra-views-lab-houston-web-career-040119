5283b263777193363168111dc462a1888dd52ee7
kreestoff <christopher.l.mcintosh@outlook.com>
2019-04-22 13:39:08 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,16 @@
 class App < Sinatra::Base
-
-	get '/' do
-		erb :index
-	end
-
-
+    get '/' do
+        erb :index
+    end
+    get '/hello' do
+        erb :hello
+    end
+    get '/goodbye' do
+        @name = 'Joe'
+        erb :goodbye
+    end
+    get '/date' do
+        @date = Time.new
+        erb :date
+    end
 end
